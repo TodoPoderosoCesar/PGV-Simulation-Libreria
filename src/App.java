290b07fc4bcff3f37ae537ea546b9921bc3f8e0f
César <usr3965@salesianos-lacuesta.net>
2025-11-17 18:32:25 +0000

    Experimento 3

--- src/App.java
+++ src/App.java
@@ -7,23 +7,30 @@ public class App {
 
         Tienda lemus = new Tienda(5);
 
-        Productor santillana = new Productor("Lemus", 5, lemus);
-    
+        Productor santillana = new Productor("Santillana", 7, lemus);
+        Productor oxford = new Productor("Oxford", 8, lemus);
+        santillana.setPriority(Thread.MAX_PRIORITY);
+        oxford.setPriority(Thread.MAX_PRIORITY);
+        
 
         Consumidor c1 = new Consumidor("Cliente_Maria", 5, lemus);
-        // Consumidor c2 = new Consumidor("Cliente_Juan", 5, lemus);
-        // Consumidor c3 = new Consumidor("TiendaCentral", 5, lemus);
+        Consumidor c2 = new Consumidor("Cliente_Juan", 5, lemus);
+        Consumidor c3 = new Consumidor("TiendaCentral", 5, lemus);
+        c1.setPriority(Thread.MIN_PRIORITY);
+        c2.setPriority(Thread.MIN_PRIORITY);
+        c3.setPriority(Thread.MIN_PRIORITY);
 
         santillana.start();
+        oxford.start();
         c1.start();
-        // c2.start();
-        // c3.start();
+        c2.start();
+        c3.start();
 
         try {
             santillana.join();
             c1.join();
-            // c2.join();
-            // c3.join();
+            c2.join();
+            c3.join();
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
